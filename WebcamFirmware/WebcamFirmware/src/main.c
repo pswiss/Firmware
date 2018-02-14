ff5d6d9dc53f613a46f9207dd9252ba482833e92
pswiss <petrasswissler2022@u.northwestern.edu>
2018-02-13 20:48:12 -0600

    Initial Project Create

---
+++ WebcamFirmware/WebcamFirmware/src/main.c
@@ -0,0 +1,10 @@
+#include <asf.h>
+
+int main (void)
+{
+	/* Insert system clock initialization code here (sysclk_init()). */
+
+	board_init();
+
+	/* Insert application code here, after the board has been initialized. */
+}
