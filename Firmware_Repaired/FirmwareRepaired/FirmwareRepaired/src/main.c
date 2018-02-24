5b379ac4d8979523ffb33bd21626d990c4509a16
pswiss <petrasswissler2022@u.northwestern.edu>
2018-02-24 11:53:07 -0600

    Updated with existing code

--- Firmware_Repaired/FirmwareRepaired/FirmwareRepaired/src/main.c
+++ Firmware_Repaired/FirmwareRepaired/FirmwareRepaired/src/main.c
@@ -1,11 +1,60 @@
-
 #include <asf.h>
+#include "wifi.h"
+#include "camera.h"
+#include "conf_board.h"
+
+#include "timer_interface.h"
+
+/** Timer counter frequency in Hz. */
+#define TC_FREQ             1
+
+
+static void configure_tc(void)
+{
+	uint32_t ul_div;
+	uint32_t ul_tcclks;
+	uint32_t ul_sysclk;
+
+	/* Get system clock. */
+	ul_sysclk = sysclk_get_cpu_hz();
+
+	/* Configure PMC. */
+	pmc_enable_periph_clk(ID_TC0);
+
+	/** Configure TC for a 1Hz frequency and trigger on RC compare. */
+	tc_find_mck_divisor(TC_FREQ, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
+	tc_init(TC0, 0, ul_tcclks | TC_CMR_CPCTRG);
+	tc_write_rc(TC0, 0, (ul_sysclk / ul_div) / TC_FREQ);
+
+	/* Configure and enable interrupt on RC compare. */
+	NVIC_EnableIRQ((IRQn_Type) ID_TC0);
+	tc_enable_interrupt(TC0, 0, TC_IER_CPCS);
+}
+
 
 int main (void)
 {
-	/* Insert system clock initialization code here (sysclk_init()). */
-
+	// boilerplate
+	sysclk_init();
+	wdt_disable(WDT);
 	board_init();
-
-	/* Insert application code here, after the board has been initialized. */
+	
+	configure_tc();
+	tc_start(TC0, 0);
+	
+	// Configure all of the wifi stuff
+	configure_usart_wifi();
+	configure_wifi_comm_pin();
+	configure_wifi_web_setup_pin();
+	
+	ioport_set_pin_level(WIFI_RESET,LOW); //reset WIFI
+	
+	/Configure all of the camera stuff
+	/*ov_init();
+	ov_configure();	*/
+	
+	while(1){
+		// do stuff
+	}
+	
 }
