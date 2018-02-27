48024f6ef49cde11ea47fe451636ff2c387b595c
wjzhouustc <36516333+wjzhouustc@users.noreply.github.com>
2018-02-26 20:36:22 -0600

    camera

--- WebcamFirmware/WebcamFirmware/src/config/camera.c
+++ WebcamFirmware/WebcamFirmware/src/config/camera.c
@@ -8,13 +8,13 @@
  camera.c contains
  void vsync_handler(uint32_t ul_id, uint32_t ul_mask)
  void init_vsync_interrupts(void)
- void configure_twi(void);
+ ?void configure_twi(void);
  void pio_capture_init(Pio *p_pio, uint32_t ul_id)
  uint8_t pio_capture_to_buffer(Pio *p_pio, uint8_t *uc_buf, uint32_t ul_size)
  void init_camera(void)
  void configure_camera(void);
  uint8_t start_capture(void)
- uint8_t find_image_len(void)
+ ?uint8_t find_image_len(void)
  
  */ 
 
